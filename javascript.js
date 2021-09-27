e759bbf5b2a299ade64166084a1428b41eb81dac
maryam shafiee <mrysheei16@gmail.com>
2021-09-27 21:59:11 +0330

    fixed search box

--- javascript.js
+++ javascript.js
@@ -1,9 +1,22 @@
 const nav = document.querySelector('.nav');
-window.addEventListener('scroll', fixNav);
- function fixNav(){
+window.addEventListener('scroll', fixNav());
+function fixNav(){
      if(window.scrollY > nav.offsetHeight + 150){
          nav.classList.add('active')
      }else{
          nav.classList.remove('active')
      }
  }
+
+const search = document.querySelector('.search');
+const btn=document.querySelector('.btn');
+const input= document.querySelector('.input');
+// btn.addEventListener('click', openSearch());
+// function openSearch(){
+//     search.classList.toggle('active');
+//     input.focus();
+// }
+btn.addEventListener('click' , () => {
+    search.classList.toggle('active')
+    input.focus();
+})
