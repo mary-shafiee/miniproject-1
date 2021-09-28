05bf2b5c9eb6d1d1beeb8465ffc8f7edd2c7335b
maryam shafiee <mrysheei16@gmail.com>
2021-09-28 19:06:40 +0330

    button ripple effect

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+////////////////////////sticky navbar///////////////////////////////////////////////////////////
 const nav = document.querySelector('.nav');
 window.addEventListener('scroll', fixNav());
 function fixNav(){
@@ -7,7 +8,7 @@ function fixNav(){
          nav.classList.remove('active')
      }
  }
-
+/////////////////////////search ////////////////////////////////////////////////////////////
 const search = document.querySelector('.search');
 const btn=document.querySelector('.btn');
 const input= document.querySelector('.input');
@@ -20,7 +21,7 @@ btn.addEventListener('click' , () => {
     search.classList.toggle('active')
     input.focus();
 })
-
+////////////////////////////////////////split landing pages///////////////////////////////////////// 
 const right = document.querySelector('.right');
 const left = document.querySelector('.left');
 const container2 = document.querySelector('.container2');
@@ -29,3 +30,28 @@ left.addEventListener('mouseenter', () => container2.classList.add('hover-left')
 left.addEventListener('mouseleave', () => container2.classList.remove('hover-left'))
 right.addEventListener('mouseenter', () => container2.classList.add('hover-right'))
 right.addEventListener('mouseleave', () => container2.classList.remove('hover-right'))
+
+/////////////////////////////////////button ripple//////////////////////////////////////////
+const buttons = document.querySelectorAll('.button')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function(e){
+        const x=e.clientX
+        const y=e.clientY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top=yInside + 'px'
+        circle.style.left= xInside +'px'
+        this.appendChild(circle)
+        setTimeout ( () => circle.remove(), 500)
+        
+
+    })
+})
