0e93afc9328543417666a7ce50683d4fd11350dc
maryam shafiee <mrysheei16@gmail.com>
2021-09-29 01:58:29 +0330

    fixed incrementing counter

--- javascript.js
+++ javascript.js
@@ -80,4 +80,23 @@ function stopSongs(){
         song.currenTime=0;
     })
 }
+/***************************************incrementing counter**************************** */
+const counters= document.querySelectorAll('.counter')
 
+counters.forEach(counter => {
+    counter.innerText='0'
+
+    const updateCounter=() => {
+        const target= +counter.getAttribute('data-target')
+        const c=+counter.innerText
+
+        const increment = target/200
+        if(c < target){
+            counter.innerText=`${Math.ceil(c+increment)}`
+            setTimeout(updateCounter,1)
+        }else{
+            counter.innerText=target
+        }
+    }
+    updateCounter()
+})
