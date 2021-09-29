fec028f7d6d3d912d925ba468f14c5cd5e7265c0
maryam shafiee <mrysheei16@gmail.com>
2021-09-29 18:57:16 +0330

    fixed animated-countdown

--- script.js
+++ script.js
@@ -35,3 +35,44 @@ function writeText(){
 }
 
 speedEL.addEventListener('input', (e) => speed =300/e.target.value)
+////////////////////////////////////////animated-countdown////////////////////////////
+const nums=document.querySelectorAll('.num span')
+const counter=document.querySelector('.counter')
+const finalMessage=document.querySelector('.final')
+const replay=document.getElementById('replay')
+
+runAnimation()
+
+function restDOM(){
+    counter.classList.remove('hide')
+    finalMessage.classList.remove('show')
+    nums.forEach((num)=> {
+        num.classList.value=''
+    })
+    nums[0].classList.add('in')
+}
+
+function runAnimation(){
+    nums.forEach((num,idx)=>{
+        const nextTolist=nums.length - 1
+
+        num.addEventListener('animationend',(e)=>{
+            if(e.runAnimation === 'goIn' && idx!== nextTolist){
+                num.classList.remove('in')
+                num.classList.add('out')
+            }else if(e.runAnimation==='goOut' && num.nextElementSibling){
+                num.nextElementSibling.classList.add('in')
+            }else{
+              
+                counter.classList.add('hide')
+                finalMessage.classList.add('show')
+            }
+        })
+    })
+}
+if(replay){
+replay.addEventListener('click' , () => {
+    restDOM()
+    runAnimation()
+    
+})}
