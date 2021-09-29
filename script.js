c24e2bcd71ac2af44ffda1a0a1aaaec3e8531ba7
maryam shafiee <mrysheei16@gmail.com>
2021-09-29 12:45:03 +0330

    fixed auto-text-effect

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+/////////////////////////////////////////////expenidng-card////////////////////////////////////////
 const panels=document.querySelectorAll('.panel')
 
 panels.forEach(panel =>{
@@ -15,3 +17,21 @@ function reamoveActiveClasses(){
     })
 
 }
+////////////////////////////////////////////auto-text-effect////////////////////////////////////////
+const textEL= document.getElementById('text')
+const speedEL=document.getElementById('speed')
+const text = 'wellcome to this page'
+let index=1
+let speed=300/speedEL.value
+writeText()
+
+function writeText(){
+    textEL.innerText = text.slice(0,index)
+    index++
+    if(index>text.length){
+        index=1
+    }
+    setTimeout(writeText,speed)
+}
+
+speedEL.addEventListener('input', (e) => speed =300/e.target.value)
